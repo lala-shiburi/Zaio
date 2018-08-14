1691130c675295bfa82d37f7b21e6780d4285d31
Nhlalala <chanellesesh@gmail.com>
2018-08-14 14:29:29 +0200

    containsDuplicate done

--- solution.php
+++ solution.php
@@ -1,3 +1,44 @@
 <?php 
-echo "This is everything";
+
+if (isset($_POST["data"])) 
+    { 
+        $usrdata = $_POST["data"];
+        $myArray = explode(',', $usrdata);
+        containsDuplicates($myArray);
+    }
+
+
+function containsDuplicates($a) {
+    $arr_length = count($a); 
+    $k=0;
+    $j=0;
+    
+    foreach ($a as $data) {
+        
+         foreach ($a as $data_0) {
+             
+             if($data==$data_0 && $k!=$j) {
+                 
+                 echo ("True");
+                
+                 exit;
+             
+             }elseif($arr_length-1 == $j){
+                    
+     
+                     echo ("False");
+                     exit;
+            }else{
+            $k++;
+            }
+             
+         }
+         $j++;
+         $k=0;
+     }
+
+     
+    }
+
+
 ?>
